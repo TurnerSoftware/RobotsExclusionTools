4c12965812c5e2ae70340eb71ee94a58652fb67b
Turnerj <git@turnerj.com>
2018-12-25 15:15:56 +1030

    Added from stream tests

--- tests/TurnerSoftware.RobotsExclusionTools.Tests/RobotsParserTests.cs
+++ tests/TurnerSoftware.RobotsExclusionTools.Tests/RobotsParserTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,16 @@ namespace TurnerSoftware.RobotsExclusionTools.Tests
 		{
 			var robotsFile = await new RobotsParser().FromUriAsync(new Uri("https://github.com/robots.txt"));
 			Assert.IsTrue(robotsFile.SiteAccessEntries.Any());
+		}
+
+		[TestMethod]
+		public async Task FromStreamLoading()
+		{
+			using (var fileStream = new FileStream("Resources/NoRobots-RFC-Example.txt", FileMode.Open))
+			{
+				var robotsFile = await new RobotsParser().FromStreamAsync(fileStream, new Uri("http://www.example.org/"));
+				Assert.IsTrue(robotsFile.SiteAccessEntries.Any());
+			}
 		}
 	}
 }
