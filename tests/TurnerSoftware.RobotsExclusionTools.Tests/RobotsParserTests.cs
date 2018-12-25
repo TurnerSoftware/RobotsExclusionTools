9b51f217788f305d838410cb11af1fc4baae5437
Turnerj <git@turnerj.com>
2018-12-25 15:04:39 +1030

    Added initial URI integration test
    I do want to swap this out for an internal web server later

---
+++ tests/TurnerSoftware.RobotsExclusionTools.Tests/RobotsParserTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TurnerSoftware.RobotsExclusionTools.Tests
+{
+	[TestClass]
+	public class RobotsParserTests : TestBase
+	{
+		[TestMethod]
+		public async Task FromUriLoading()
+		{
+			var robotsFile = await new RobotsParser().FromUriAsync(new Uri("https://github.com/robots.txt"));
+			Assert.IsTrue(robotsFile.SiteAccessEntries.Any());
+		}
+	}
+}
