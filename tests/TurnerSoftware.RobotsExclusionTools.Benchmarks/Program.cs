c1f87f63f99b9dc50a56f56f434bc4ffda29f6af
Turnerj <git@turnerj.com>
2018-12-26 17:16:16 +1030

    Initial benchmark project

---
+++ tests/TurnerSoftware.RobotsExclusionTools.Benchmarks/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using BenchmarkDotNet.Running;
+
+namespace TurnerSoftware.RobotsExclusionTools.Benchmarks
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var summary = BenchmarkRunner.Run<StreamingVsStringTokenization>();
+		}
+	}
+}
---
+++ tests/TurnerSoftware.RobotsExclusionTools.Benchmarks/StreamingVsStringTokenization.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+
+namespace TurnerSoftware.RobotsExclusionTools.Benchmarks
+{
+	[ClrJob]
+	[MemoryDiagnoser]
+	public class StreamingVsStringTokenization
+	{
+		private RobotsParser Parser { get; }
+		private Uri Uri { get; }
+
+		public StreamingVsStringTokenization()
+		{
+			Parser = new RobotsParser();
+			Uri = new Uri("https://www.google.com");
+		}
+
+		[Benchmark]
+		public async Task StreamingTokenization()
+		{
+			using (var fileStream = new FileStream("Resources/Google-Robots.txt", FileMode.Open))
+			{
+				var result = await Parser.FromStreamAsync(fileStream, Uri);
+			}
+		}
+
+		[Benchmark]
+		public async Task StringTokenization()
+		{
+			string robots;
+
+			using (var fileStream = new FileStream("Resources/Google-Robots.txt", FileMode.Open))
+			{
+				using (var streamReader = new StreamReader(fileStream))
+				{
+					robots = await streamReader.ReadToEndAsync();
+				}
+			}
+			
+			var result = Parser.FromString(robots, Uri);
+		}
+	}
+}
