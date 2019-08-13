4d279de5cb8956539df370d37a5e4acfec66645e
Turnerj <git@turnerj.com>
2019-08-13 12:04:31 +0930

    Fixed bugs in regex for tokenizing

--- src/TurnerSoftware.RobotsExclusionTools/Tokenization/RobotsFileTokenizer.cs
+++ src/TurnerSoftware.RobotsExclusionTools/Tokenization/RobotsFileTokenizer.cs
@@ -33,11 +33,11 @@ namespace TurnerSoftware.RobotsExclusionTools.Tokenization
 			//This can be expressed as the following:
 			//\x21\x23-\x27\x2a\x2b\x2d\x2e\x41-\x5a\x5e-\x7a\x7c\x7e
 
-			new TokenDefinition(TokenType.Comment, "^#[^\x0A\x0D]*"),
-			new TokenDefinition(TokenType.Field, "^[\x21\x23-\x27\x2a\x2b\x2d\x2e\x41-\x5a\x5e-\x7a\x7c\x7e]+(?=:[ ])"),
+			new TokenDefinition(TokenType.Comment, @"^#[^\x0A\x0D]*"),
+			new TokenDefinition(TokenType.Field, @"^[\x21\x23-\x27\x2a\x2b\x2d\x2e\x41-\x5a\x5e-\x7a\x7c\x7e]+(?=:[ ])"),
 			new TokenDefinition(TokenType.FieldValueDelimiter, "^:[ ]"),
-			new TokenDefinition(TokenType.Value, "^[^\x0A\x0D#]+"),
-			new TokenDefinition(TokenType.NewLine, "^\x0D?\x0A")
+			new TokenDefinition(TokenType.Value, @"^[^\x0A\x0D#]+"),
+			new TokenDefinition(TokenType.NewLine, @"^\x0D?\x0A")
 		};
 
 		protected override IEnumerable<TokenDefinition> GetTokenDefinitions()
--- src/TurnerSoftware.RobotsExclusionTools/Tokenization/RobotsPageTokenizer.cs
+++ src/TurnerSoftware.RobotsExclusionTools/Tokenization/RobotsPageTokenizer.cs
@@ -9,10 +9,10 @@ namespace TurnerSoftware.RobotsExclusionTools.Tokenization
 		private static readonly IEnumerable<TokenDefinition> TokenDefinitions = new List<TokenDefinition>
 		{
 			//Based on the same character restriction rules as the RobotsFileTokenizer
-			new TokenDefinition(TokenType.Field, "^[\x21\x23-\x27\x2a\x2b\x2d\x2e\x41-\x5a\x5e-\x7a\x7c\x7e]+(?=:[ ])"),
+			new TokenDefinition(TokenType.Field, @"^[\x21\x23-\x27\x2a\x2b\x2d\x2e\x41-\x5a\x5e-\x7a\x7c\x7e]+(?=:[ ])"),
 			new TokenDefinition(TokenType.FieldValueDelimiter, "^:[ ]"),
-			new TokenDefinition(TokenType.Value, "^[^\x0A\x0D#]+"),
-			new TokenDefinition(TokenType.NewLine, "^\x0D?\x0A"),
+			new TokenDefinition(TokenType.Value, @"^[^\x0A\x0D#]+"),
+			new TokenDefinition(TokenType.NewLine, @"^\x0D?\x0A"),
 			new TokenDefinition(TokenType.ValueDelimiter, "^,[ ]+")
 		};
 		
