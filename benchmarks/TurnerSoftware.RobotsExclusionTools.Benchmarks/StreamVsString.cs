312a4a05113df3599d4c31f6b2899ac910b44687
Turnerj <git@turnerj.com>
2022-04-04 21:22:51 +0930

    Updated benchmarks

---
+++ benchmarks/TurnerSoftware.RobotsExclusionTools.Benchmarks/StreamVsString.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+
+namespace TurnerSoftware.RobotsExclusionTools.Benchmarks;
+
+[Config(typeof(DefaultBenchmarkConfig))]
+public class StreamVsString
+{
+	private RobotsFileParser Parser { get; } = new RobotsFileParser();
+	private Uri Uri { get; } = new Uri("https://google.com");
+
+	private MemoryStream MemoryStream { get; set; }
+	private string RobotsText { get; set; }
+
+	[GlobalSetup]
+	public void Setup()
+	{
+		MemoryStream = new MemoryStream();
+
+		using (var fileStream = new FileStream("Resources/Google-Robots.txt", FileMode.Open))
+		{
+			fileStream.CopyTo(MemoryStream);
+		}
+
+		MemoryStream.Seek(0, SeekOrigin.Begin);
+		var streamReader = new StreamReader(MemoryStream);
+		RobotsText = streamReader.ReadToEnd();
+		MemoryStream.Seek(0, SeekOrigin.Begin);
+	}
+
+	[Benchmark]
+	public async Task<RobotsFile> FromStream()
+	{
+		var result = await Parser.FromStreamAsync(MemoryStream, Uri);
+		MemoryStream.Seek(0, SeekOrigin.Begin);
+		return result;
+	}
+
+	[Benchmark]
+	public RobotsFile FromString()
+	{
+		return Parser.FromString(RobotsText, Uri);
+	}
+}
