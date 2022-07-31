c70b4e0813f10d19bb4150d621c19e687b363b44
Turnerj <git@turnerj.com>
2022-07-31 22:18:36 +0930

    Initial implementation of stream reading

---
+++ src/TurnerSoftware.RobotsExclusionTools/Helpers/StreamLineReader.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Pipelines;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TurnerSoftware.RobotsExclusionTools.Helpers;
+
+public static class StreamLineReader
+{
+	private const byte NewLineChar = (byte)'\n';
+
+	public static IAsyncEnumerable<ReadOnlySequence<byte>> EnumerateLinesAsync(Stream stream, CancellationToken cancellationToken = default)
+	{
+		var pipe = new Pipe();
+		var pipeWriterTask = FillPipeAsync(stream, pipe.Writer, cancellationToken);
+		return ReadPipeAsync(pipeWriterTask, pipe.Reader, cancellationToken);
+	}
+
+	private static async Task FillPipeAsync(Stream stream, PipeWriter writer, CancellationToken cancellationToken)
+	{
+		await stream.CopyToAsync(writer, cancellationToken);
+		writer.Complete();
+	}
+
+	private static async IAsyncEnumerable<ReadOnlySequence<byte>> ReadPipeAsync(
+		Task pipeWriterTask, 
+		PipeReader reader, 
+		[EnumeratorCancellation] CancellationToken cancellationToken
+	)
+	{
+		while (true)
+		{
+			var result = await reader.ReadAsync(cancellationToken);
+			var buffer = result.Buffer;
+			SequencePosition? position;
+			do
+			{
+				position = buffer.PositionOf(NewLineChar);
+
+				if (position != null)
+				{
+					yield return buffer.Slice(0, position.Value);
+					buffer = buffer.Slice(buffer.GetPosition(1, position.Value));
+				}
+			}
+			while (position != null);
+
+			reader.AdvanceTo(buffer.Start, buffer.End);
+
+			if (result.IsCompleted)
+			{
+				if (!buffer.IsEmpty)
+				{
+					yield return buffer;
+				}
+				break;
+			}
+		}
+
+		reader.Complete();
+		await pipeWriterTask;
+	}
+}
--- src/TurnerSoftware.RobotsExclusionTools/RobotsFileParser.cs
+++ src/TurnerSoftware.RobotsExclusionTools/RobotsFileParser.cs
@@ -7,6 +7,9 @@ using System.Net;
 using System.Threading;
 using System.Runtime.CompilerServices;
 using TurnerSoftware.RobotsExclusionTools.Tokenization;
+using System.Buffers;
+using TurnerSoftware.RobotsExclusionTools.Helpers;
+using System.Text;
 
 namespace TurnerSoftware.RobotsExclusionTools;
 
@@ -24,7 +27,8 @@ public class RobotsFileParser : IRobotsFileParser
 	/// <inheritdoc/>
 	public RobotsFile FromString(string robotsText, Uri baseUri)
 	{
-		Parse(robotsText.AsMemory(), out var siteAccessEntries, out var sitemapEntries);
+		//Parse(robotsText.AsMemory(), out var siteAccessEntries, out var sitemapEntries);
+		Parse2(robotsText.AsMemory(), out var siteAccessEntries, out var sitemapEntries);
 		return new RobotsFile(baseUri, siteAccessEntries, sitemapEntries);
 	}
 
@@ -70,9 +74,54 @@ public class RobotsFileParser : IRobotsFileParser
 	/// <inheritdoc/>
 	public async Task<RobotsFile> FromStreamAsync(Stream stream, Uri baseUri, CancellationToken cancellationToken = default)
 	{
-		var streamReader = new StreamReader(stream);
-		var robotsText = await streamReader.ReadToEndAsync();
-		return FromString(robotsText, baseUri);
+		var parseState = SiteAccessParseState.Create();
+
+		var tmpSiteAccessEntries = new List<SiteAccessEntry>();
+		var tmpSitemapEntries = new List<SitemapUrlEntry>();
+
+		RobotsFileToken lastToken = default;
+
+		await foreach (var lineSequence in StreamLineReader.EnumerateLinesAsync(stream, cancellationToken))
+		{
+			var numberOfBytes = (int)lineSequence.Length;
+			var byteArray = ArrayPool<byte>.Shared.Rent(numberOfBytes);
+			var charArray = ArrayPool<char>.Shared.Rent(numberOfBytes);
+
+			try
+			{
+				lineSequence.CopyTo(byteArray.AsSpan(0, numberOfBytes));
+				var numberOfChars = Encoding.UTF8.GetChars(byteArray, 0, numberOfBytes, charArray, 0);
+				var charLine = charArray.AsMemory(0, numberOfChars);
+
+				//Do work here
+				var reader = new RobotsFileTokenReader(charLine);
+				if (reader.NextToken(out var token, RobotsFileTokenValueFormat.RuleName))
+				{
+					var (accessEntry, sitemapEntry) = ProcessLine(token, ref reader, ref parseState, ref lastToken);
+					if (accessEntry.HasValue)
+					{
+						tmpSiteAccessEntries.Add(accessEntry.Value);
+					}
+					else if (sitemapEntry.HasValue)
+					{
+						tmpSitemapEntries.Add(sitemapEntry.Value);
+					}
+				}
+			}
+			finally
+			{
+				ArrayPool<byte>.Shared.Return(byteArray);
+				ArrayPool<char>.Shared.Return(charArray);
+			}
+		}
+
+		//Add final entry that was being constructed in our state tracker
+		if (parseState.TryGetEntry(out var finalAccessEntry))
+		{
+			tmpSiteAccessEntries.Add(finalAccessEntry);
+		}
+
+		return new RobotsFile(baseUri, tmpSiteAccessEntries, tmpSitemapEntries);
 	}
 
 	private struct SiteAccessParseState
@@ -92,6 +141,23 @@ public class RobotsFileParser : IRobotsFileParser
 			UserAgents.Clear();
 			PathRules.Clear();
 			CrawlDelay = null;
+		}
+
+		public bool TryGetEntry(out SiteAccessEntry accessEntry)
+		{
+			if (UserAgents.Count == 0)
+			{
+				accessEntry = default;
+				return false;
+			}
+
+			accessEntry = new SiteAccessEntry
+			{
+				UserAgents = UserAgents.ToArray(),
+				PathRules = PathRules.Count > 0 ? PathRules.ToArray() : Array.Empty<SiteAccessPathRule>(),
+				CrawlDelay = CrawlDelay
+			};
+			return true;
 		}
 
 		public SiteAccessEntry AsEntry()
@@ -281,5 +347,176 @@ public class RobotsFileParser : IRobotsFileParser
 
 		siteAccessEntries = tmpSiteAccessEntries;
 		sitemapEntries = tmpSitemapEntries;
+	}
+
+	private void Parse2(ReadOnlyMemory<char> value, out IReadOnlyCollection<SiteAccessEntry> siteAccessEntries, out IReadOnlyCollection<SitemapUrlEntry> sitemapEntries)
+	{
+		var reader = new RobotsFileTokenReader(value);
+		var parseState = SiteAccessParseState.Create();
+
+		var tmpSiteAccessEntries = new List<SiteAccessEntry>();
+		var tmpSitemapEntries = new List<SitemapUrlEntry>();
+
+		RobotsFileToken lastToken = default;
+
+		while (reader.NextToken(out var token, RobotsFileTokenValueFormat.RuleName))
+		{
+			var (accessEntry, sitemapEntry) = ProcessLine(token, ref reader, ref parseState, ref lastToken);
+			if (accessEntry.HasValue)
+			{
+				tmpSiteAccessEntries.Add(accessEntry.Value);
+			}
+			else if (sitemapEntry.HasValue)
+			{
+				tmpSitemapEntries.Add(sitemapEntry.Value);
+			}
+		}
+
+		//Add final entry that was being constructed in our state tracker
+		if (parseState.TryGetEntry(out var finalAccessEntry))
+		{
+			tmpSiteAccessEntries.Add(finalAccessEntry);
+		}
+
+		siteAccessEntries = tmpSiteAccessEntries;
+		sitemapEntries = tmpSitemapEntries;
+	}
+
+	private static (SiteAccessEntry? accessEntry, SitemapUrlEntry? sitemapEntry) ProcessLine(RobotsFileToken token, ref RobotsFileTokenReader reader, ref SiteAccessParseState parseState, ref RobotsFileToken lastToken)
+	{
+		SiteAccessEntry? accessEntry = default;
+		SitemapUrlEntry? sitemapEntry = default;
+
+		if (token.TokenType is RobotsFileTokenType.Whitespace && !reader.NextToken(out token, RobotsFileTokenValueFormat.RuleName))
+		{
+			goto RejectLine;
+		}
+
+		switch (token.TokenType)
+		{
+			case RobotsFileTokenType.Value:
+				var tokenValue = token.Value.Span;
+				if (AreEqual(tokenValue, Constants.DisallowField) || AreEqual(tokenValue, Constants.AllowField))
+				{
+					var ruleType = PathRuleType.Disallow;
+					if (tokenValue[0] is 'A' or 'a')
+					{
+						ruleType = PathRuleType.Allow;
+					}
+
+					//When we have seen a field for the first time that isn't a "User-agent", default to any user agent (written as "*")
+					if (lastToken.Value.IsEmpty && parseState.UserAgents.Count == 0)
+					{
+						parseState.UserAgents.Add(Constants.UserAgentWildcard);
+					}
+
+					//Ensure our next token is a delimiter, otherwise skip the entire line
+					if (!reader.NextToken(out var expectedDelimiter) || expectedDelimiter.TokenType != RobotsFileTokenType.Delimiter)
+					{
+						reader.SkipLine();
+						goto RejectLine;
+					}
+
+					//As long as we have a delimiter, we can accept this as a blank value if there are no more tokens
+					if (!reader.NextToken(out token))
+					{
+						goto AcceptBlankValue;
+					}
+
+					//Step over whitespace (though if we are at the end, accept it as a blank value)
+					if (token.TokenType == RobotsFileTokenType.Whitespace && !reader.NextToken(out token))
+					{
+						goto AcceptBlankValue;
+					}
+
+					switch (token.TokenType)
+					{
+						//We can have an empty value - we detect this by a comment or a new line token
+						case RobotsFileTokenType.NewLine:
+						case RobotsFileTokenType.Comment:
+							goto AcceptBlankValue;
+						//If we have a value, check it as a path
+						case RobotsFileTokenType.Value when token.IsValidPath():
+							parseState.PathRules.Add(new SiteAccessPathRule
+							{
+								RuleType = ruleType,
+								Path = token.ToString()
+							});
+							goto AcceptToken;
+						//Anything else, we ignore the entire declaration
+						default:
+							reader.SkipLine();
+							goto RejectLine;
+					}
+
+					AcceptBlankValue:
+					parseState.PathRules.Add(new SiteAccessPathRule(string.Empty, ruleType));
+					goto AcceptToken;
+				}
+				else if (AreEqual(tokenValue, Constants.UserAgentField))
+				{
+					//Reset the state when we have encountered a new "User-agent" field not immediately after another
+					if (!lastToken.Value.IsEmpty && !AreEqual(lastToken.Value.Span, Constants.UserAgentField))
+					{
+						if (parseState.TryGetEntry(out var localAccessEntry))
+						{
+							accessEntry = localAccessEntry;
+							parseState.Reset();
+						}
+					}
+
+					//The value format "token" is the same syntax as "agent"
+					if (SkipFieldToValue(ref reader, out token))
+					{
+						parseState.UserAgents.Add(token.ToString());
+						goto AcceptToken;
+					}
+
+					reader.SkipLine();
+					goto RejectLine;
+				}
+				else if (AreEqual(tokenValue, Constants.CrawlDelayField))
+				{
+					if (SkipFieldToValue(ref reader, out token))
+					{
+						var localTokenValue = token.Value.Span;
+#if NETSTANDARD2_0
+						if (int.TryParse(localTokenValue.ToString(), out var parsedCrawlDelay))
+#else
+						//We can have allocation-free integer parsing in .NET Standard 2.1
+						if (int.TryParse(localTokenValue, out var parsedCrawlDelay))
+#endif
+						{
+							parseState.CrawlDelay = parsedCrawlDelay;
+						}
+					}
+				}
+				else if (AreEqual(tokenValue, Constants.SitemapField))
+				{
+					if (SkipFieldToValue(ref reader, out token))
+					{
+						var tokenString = token.Value.Span.ToString();
+						if (Uri.TryCreate(tokenString, UriKind.Absolute, out var parsedUri))
+						{
+							sitemapEntry = new SitemapUrlEntry(parsedUri);
+							goto AcceptToken;
+						}
+					}
+					reader.SkipLine();
+					goto RejectLine;
+				}
+				break;
+			case RobotsFileTokenType.NewLine:
+				goto AcceptToken;
+			default:
+				//If it doesn't start with a value, skip the line
+				reader.SkipLine();
+				goto RejectLine;
+		}
+		AcceptToken:
+		lastToken = token;
+		return (accessEntry, sitemapEntry);
+		RejectLine:
+		return (default, default);
 	}
 }
