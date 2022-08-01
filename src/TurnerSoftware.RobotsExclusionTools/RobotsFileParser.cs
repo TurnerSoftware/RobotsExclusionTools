c2f4bc228b774a04c19e12c0e342e0d032aa0d72
Turnerj <git@turnerj.com>
2022-08-01 21:58:51 +0930

    Cleaning up code

--- src/TurnerSoftware.RobotsExclusionTools/RobotsFileParser.cs
+++ src/TurnerSoftware.RobotsExclusionTools/RobotsFileParser.cs
@@ -25,34 +25,38 @@ public class RobotsFileParser : IRobotsFileParser
 	}
 
 	/// <inheritdoc/>
+	public async Task<RobotsFile> FromStreamAsync(Stream stream, Uri baseUri, CancellationToken cancellationToken = default)
+	{
+		var parseState = new SiteAccessParseState();
+
+		await foreach (var charLine in StreamLineReader.EnumerateLinesOfCharsAsync(stream, cancellationToken))
+		{
+			var reader = new RobotsFileTokenReader(charLine);
+			if (reader.NextToken(out var token, RobotsFileTokenValueFormat.RuleName))
+			{
+				ProcessLine(token, ref reader, ref parseState);
+			}
+		}
+
+		//Capture final entry that was being constructed in our state tracker
+		parseState.CaptureEntry();
+		return new RobotsFile(baseUri, parseState.SiteAccessEntries, parseState.SitemapUrlEntries);
+	}
+
+	/// <inheritdoc/>
 	public RobotsFile FromString(string robotsText, Uri baseUri)
 	{
 		var reader = new RobotsFileTokenReader(robotsText.AsMemory());
-		var parseState = SiteAccessParseState.Create();
-
-		var tmpSiteAccessEntries = new List<SiteAccessEntry>();
-		var tmpSitemapEntries = new List<SitemapUrlEntry>();
+		var parseState = new SiteAccessParseState();
 
 		while (reader.NextToken(out var token, RobotsFileTokenValueFormat.RuleName))
 		{
-			var (accessEntry, sitemapEntry) = ProcessLine(token, ref reader, ref parseState);
-			if (accessEntry.HasValue)
-			{
-				tmpSiteAccessEntries.Add(accessEntry.Value);
-			}
-			else if (sitemapEntry.HasValue)
-			{
-				tmpSitemapEntries.Add(sitemapEntry.Value);
-			}
+			ProcessLine(token, ref reader, ref parseState);
 		}
 
 		//Add final entry that was being constructed in our state tracker
-		if (parseState.TryGetEntry(out var finalAccessEntry))
-		{
-			tmpSiteAccessEntries.Add(finalAccessEntry);
-		}
-
-		return new RobotsFile(baseUri, tmpSiteAccessEntries, tmpSitemapEntries);
+		parseState.CaptureEntry();
+		return new RobotsFile(baseUri, parseState.SiteAccessEntries, parseState.SitemapUrlEntries);
 	}
 
 	/// <inheritdoc/>
@@ -94,93 +98,58 @@ public class RobotsFileParser : IRobotsFileParser
 		return RobotsFile.AllowAllRobots(baseUri);
 	}
 
-	/// <inheritdoc/>
-	public async Task<RobotsFile> FromStreamAsync(Stream stream, Uri baseUri, CancellationToken cancellationToken = default)
-	{
-		var parseState = SiteAccessParseState.Create();
-
-		var tmpSiteAccessEntries = new List<SiteAccessEntry>();
-		var tmpSitemapEntries = new List<SitemapUrlEntry>();
-
-		await foreach (var charLine in StreamLineReader.EnumerateLinesOfCharsAsync(stream, cancellationToken))
-		{
-			var reader = new RobotsFileTokenReader(charLine);
-			if (reader.NextToken(out var token, RobotsFileTokenValueFormat.RuleName))
-			{
-				var (accessEntry, sitemapEntry) = ProcessLine(token, ref reader, ref parseState);
-				if (accessEntry.HasValue)
-				{
-					tmpSiteAccessEntries.Add(accessEntry.Value);
-				}
-				else if (sitemapEntry.HasValue)
-				{
-					tmpSitemapEntries.Add(sitemapEntry.Value);
-				}
-			}
-		}
-
-		//Add final entry that was being constructed in our state tracker
-		if (parseState.TryGetEntry(out var finalAccessEntry))
-		{
-			tmpSiteAccessEntries.Add(finalAccessEntry);
-		}
-
-		return new RobotsFile(baseUri, tmpSiteAccessEntries, tmpSitemapEntries);
-	}
-
 	private struct SiteAccessParseState
 	{
-		public List<string> UserAgents { get; private set; }
-		public List<SiteAccessPathRule> PathRules { get; private set; }
+		public List<string> UserAgents { get; }
+		public List<SiteAccessPathRule> PathRules { get; }
 		public int? CrawlDelay { get; set; }
 		public bool HasSeenRule { get; set; }
 
-		public static SiteAccessParseState Create() => new()
-		{
-			UserAgents = new List<string>(),
-			PathRules = new List<SiteAccessPathRule>()
-		};
+		private readonly List<SiteAccessEntry> siteAccessEntries;
+		public IReadOnlyList<SiteAccessEntry> SiteAccessEntries => siteAccessEntries;
 
-		public void Reset()
+		public List<SitemapUrlEntry> SitemapUrlEntries { get; }
+
+		public SiteAccessParseState()
 		{
+			UserAgents = new();
+			PathRules = new();
+			CrawlDelay = null;
+			HasSeenRule = false;
+
+			siteAccessEntries = new();
+			SitemapUrlEntries = new();
+		}
+
+		/// <summary>
+		/// Captures the currently tracked entry and resets the state to start tracking another.
+		/// </summary>
+		/// <remarks>
+		/// A tracked entry is only captured if there are tracked user-agents and any rule has been seen.
+		/// </remarks>
+		public void CaptureEntry()
+		{
+			if (UserAgents.Count > 0 && HasSeenRule)
+			{
+				siteAccessEntries.Add(new SiteAccessEntry
+				{
+					UserAgents = UserAgents.ToArray(),
+					PathRules = PathRules.Count > 0 ? PathRules.ToArray() : Array.Empty<SiteAccessPathRule>(),
+					CrawlDelay = CrawlDelay
+				});
+			}
+
 			UserAgents.Clear();
 			PathRules.Clear();
 			CrawlDelay = null;
 			HasSeenRule = false;
-		}
-
-		public bool TryGetEntry(out SiteAccessEntry accessEntry)
-		{
-			if (UserAgents.Count == 0)
-			{
-				accessEntry = default;
-				return false;
-			}
-
-			accessEntry = new SiteAccessEntry
-			{
-				UserAgents = UserAgents.ToArray(),
-				PathRules = PathRules.Count > 0 ? PathRules.ToArray() : Array.Empty<SiteAccessPathRule>(),
-				CrawlDelay = CrawlDelay
-			};
-			return true;
-		}
-
-		public SiteAccessEntry AsEntry()
-		{
-			return new SiteAccessEntry
-			{
-				UserAgents = UserAgents.ToArray(),
-				PathRules = PathRules.ToArray(),
-				CrawlDelay = CrawlDelay
-			};
 		}
 	}
 
 	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	private static bool AreEqual(ReadOnlySpan<char> input, string comparison) => input.Equals(comparison.AsSpan(), StringComparison.OrdinalIgnoreCase);
 
-	private static bool SkipFieldToValue(ref RobotsFileTokenReader reader, out RobotsFileToken token)
+	private static bool TrySkipFieldToValue(ref RobotsFileTokenReader reader, out RobotsFileToken token)
 	{
 		if (!reader.NextToken(out var expectedDelimiter) || expectedDelimiter.TokenType != RobotsFileTokenType.Delimiter)
 		{
@@ -212,135 +181,119 @@ public class RobotsFileParser : IRobotsFileParser
 		return false;
 	}
 
-	private static (SiteAccessEntry? accessEntry, SitemapUrlEntry? sitemapEntry) ProcessLine(RobotsFileToken token, ref RobotsFileTokenReader reader, ref SiteAccessParseState parseState)
+	private static void ProcessLine(RobotsFileToken token, ref RobotsFileTokenReader reader, ref SiteAccessParseState parseState)
 	{
-		SiteAccessEntry? accessEntry = default;
-		SitemapUrlEntry? sitemapEntry = default;
-
 		if (token.TokenType is RobotsFileTokenType.Whitespace && !reader.NextToken(out token, RobotsFileTokenValueFormat.RuleName))
 		{
-			goto RejectLine;
+			return;
 		}
-
-		switch (token.TokenType)
+		else if (token.TokenType is RobotsFileTokenType.Value)
 		{
-			case RobotsFileTokenType.Value:
-				var tokenValue = token.Value.Span;
-				if (AreEqual(tokenValue, Constants.UserAgentField))
+			var tokenValue = token.Value.Span;
+			if (AreEqual(tokenValue, Constants.UserAgentField))
+			{
+				parseState.CaptureEntry();
+
+				if (TrySkipFieldToValue(ref reader, out token) && token.IsValidIdentifier())
 				{
-					//Reset the state when we have encountered a new "User-agent" field not immediately after another
-					if (parseState.HasSeenRule && parseState.TryGetEntry(out var localAccessEntry))
-					{
-						accessEntry = localAccessEntry;
-						parseState.Reset();
-					}
-
-					if (SkipFieldToValue(ref reader, out token) && token.IsValidIdentifier())
-					{
-						parseState.UserAgents.Add(token.ToString());
-						goto AcceptToken;
-					}
-
-					reader.SkipLine();
-					goto RejectLine;
+					parseState.UserAgents.Add(token.ToString());
+					return;
 				}
-				else
-				{
-					parseState.HasSeenRule = true;
-					if (AreEqual(tokenValue, Constants.DisallowField) || AreEqual(tokenValue, Constants.AllowField))
-					{
-						var ruleType = PathRuleType.Disallow;
-						if (tokenValue[0] is 'A' or 'a')
-						{
-							ruleType = PathRuleType.Allow;
-						}
 
-						//When we have seen a field for the first time that isn't a "User-agent", default to any user agent (written as "*")
-						if (parseState.UserAgents.Count == 0)
-						{
-							parseState.UserAgents.Add(Constants.UserAgentWildcard);
-						}
-
-						//Ensure our next token is a delimiter, otherwise skip the entire line
-						if (!reader.NextToken(out var expectedDelimiter) || expectedDelimiter.TokenType != RobotsFileTokenType.Delimiter)
-						{
-							reader.SkipLine();
-							goto RejectLine;
-						}
-
-						//As long as we have a delimiter, we can accept this as a blank value if there are no more tokens
-						if (!reader.NextToken(out token))
-						{
-							goto AcceptBlankValue;
-						}
-
-						//Step over whitespace (though if we are at the end, accept it as a blank value)
-						if (token.TokenType == RobotsFileTokenType.Whitespace && !reader.NextToken(out token))
-						{
-							goto AcceptBlankValue;
-						}
-
-						switch (token.TokenType)
-						{
-							//We can have an empty value - we detect this by a comment or a new line token
-							case RobotsFileTokenType.NewLine:
-							case RobotsFileTokenType.Comment:
-								goto AcceptBlankValue;
-							//If we have a value, check it as a path
-							case RobotsFileTokenType.Value when token.IsValidPath():
-								parseState.PathRules.Add(new SiteAccessPathRule
-								{
-									RuleType = ruleType,
-									Path = token.ToString()
-								});
-								goto AcceptToken;
-							//Anything else, we ignore the entire declaration
-							default:
-								reader.SkipLine();
-								goto RejectLine;
-						}
-
-						AcceptBlankValue:
-						parseState.PathRules.Add(new SiteAccessPathRule(string.Empty, ruleType));
-						goto AcceptToken;
-					}
-					else if (AreEqual(tokenValue, Constants.CrawlDelayField))
-					{
-						if (SkipFieldToValue(ref reader, out token))
-						{
-							var localTokenValue = token.Value.Span;
-							if (PerfUtilities.TryParseInteger(localTokenValue, out var parsedCrawlDelay))
-							{
-								parseState.CrawlDelay = parsedCrawlDelay;
-							}
-						}
-					}
-					else if (AreEqual(tokenValue, Constants.SitemapField))
-					{
-						if (SkipFieldToValue(ref reader, out token))
-						{
-							var tokenString = token.Value.Span.ToString();
-							if (Uri.TryCreate(tokenString, UriKind.Absolute, out var parsedUri))
-							{
-								sitemapEntry = new SitemapUrlEntry(parsedUri);
-								goto AcceptToken;
-							}
-						}
-						reader.SkipLine();
-						goto RejectLine;
-					}
-				}
-				break;
-			case RobotsFileTokenType.NewLine:
-				goto AcceptToken;
-			default:
-				//If it doesn't start with a value, skip the line
 				reader.SkipLine();
-				goto RejectLine;
+			}
+			else
+			{
+				if (AreEqual(tokenValue, Constants.DisallowField) || AreEqual(tokenValue, Constants.AllowField))
+				{
+					var ruleType = PathRuleType.Disallow;
+					if (tokenValue[0] is 'A' or 'a')
+					{
+						ruleType = PathRuleType.Allow;
+					}
+
+					//When we have seen a field for the first time that isn't a "User-agent", default to any user agent (written as "*")
+					if (parseState.UserAgents.Count == 0)
+					{
+						parseState.UserAgents.Add(Constants.UserAgentWildcard);
+					}
+
+					//Ensure our next token is a delimiter, otherwise skip the entire line
+					if (!reader.NextToken(out var expectedDelimiter) || expectedDelimiter.TokenType != RobotsFileTokenType.Delimiter)
+					{
+						reader.SkipLine();
+						return;
+					}
+
+					//As long as we have a delimiter, we can accept this as a blank value if there are no more tokens
+					if (!reader.NextToken(out token))
+					{
+						goto AcceptBlankValue;
+					}
+
+					//Step over whitespace (though if we are at the end, accept it as a blank value)
+					if (token.TokenType == RobotsFileTokenType.Whitespace && !reader.NextToken(out token))
+					{
+						goto AcceptBlankValue;
+					}
+
+					switch (token.TokenType)
+					{
+						//We can have an empty value - we detect this by a comment or a new line token
+						case RobotsFileTokenType.NewLine:
+						case RobotsFileTokenType.Comment:
+							goto AcceptBlankValue;
+						//If we have a value, check it as a path
+						case RobotsFileTokenType.Value when token.IsValidPath():
+							parseState.HasSeenRule = true;
+							parseState.PathRules.Add(new SiteAccessPathRule
+							{
+								RuleType = ruleType,
+								Path = token.ToString()
+							});
+							return;
+						//Anything else, we ignore the entire declaration
+						default:
+							reader.SkipLine();
+							return;
+					}
+
+					AcceptBlankValue:
+					parseState.HasSeenRule = true;
+					parseState.PathRules.Add(new SiteAccessPathRule(string.Empty, ruleType));
+				}
+				else if (AreEqual(tokenValue, Constants.CrawlDelayField))
+				{
+					if (TrySkipFieldToValue(ref reader, out token))
+					{
+						var localTokenValue = token.Value.Span;
+						if (PerfUtilities.TryParseInteger(localTokenValue, out var parsedCrawlDelay))
+						{
+							parseState.HasSeenRule = true;
+							parseState.CrawlDelay = parsedCrawlDelay;
+						}
+					}
+				}
+				else if (AreEqual(tokenValue, Constants.SitemapField))
+				{
+					if (TrySkipFieldToValue(ref reader, out token))
+					{
+						var tokenString = token.Value.Span.ToString();
+						if (Uri.TryCreate(tokenString, UriKind.Absolute, out var parsedUri))
+						{
+							parseState.HasSeenRule = true;
+							parseState.SitemapUrlEntries.Add(new SitemapUrlEntry(parsedUri));
+							return;
+						}
+					}
+					reader.SkipLine();
+				}
+			}
 		}
-		AcceptToken:
-		return (accessEntry, sitemapEntry);
-		RejectLine:
-		return (default, default);
+		else if (token.TokenType is not RobotsFileTokenType.NewLine)
+		{
+			//If it doesn't start with a value, skip the line
+			reader.SkipLine();
+		}
 	}
 }
