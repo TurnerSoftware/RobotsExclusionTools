913e0379026f8b7b3fc894721058e7623a3f118f
Turnerj <git@turnerj.com>
2022-08-04 23:58:10 +0930

    Add IsValidPath benchmark

---
+++ benchmarks/TurnerSoftware.RobotsExclusionTools.Benchmarks/IsValidPathBenchmark.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+using BenchmarkDotNet.Attributes;
+using TurnerSoftware.RobotsExclusionTools.Helpers;
+
+namespace TurnerSoftware.RobotsExclusionTools.Benchmarks;
+
+[Config(typeof(IntrinsicBenchmarkConfig))]
+public class IsValidPathBenchmark
+{
+	[Params("/about", "/wwwroot/html/assets/logo.png")]
+	public string Path;
+
+	private byte[] PathData;
+
+	[GlobalSetup]
+	public void Setup()
+	{
+		PathData = Encoding.UTF8.GetBytes(Path);
+	}
+
+	[Benchmark]
+	public bool IsValidPath() => RobotsExclusionProtocolHelper.IsValidPath(PathData);
+}
